d10ff302bae031aa35c6b508dbb3b9b9fcd437cc
William Mironchuk <wmironchuk@umass.edu>
2022-02-07 10:29:25 -0500

    Finished the main function, and began work on the rest of the program
    created open_file_and_parse function prototype, but have yet to create
    the files themselves

--- hexdump.py
+++ hexdump.py
@@ -3,7 +3,7 @@ from argparse import ArgumentParser
 
 def main():
     args = ArgumentParser(
-            discription="display file contents in headecimal and ascii"
+            description="display file contents in headecimal and ascii"
             )
 
     args.add_argument(
@@ -11,6 +11,12 @@ def main():
             type=str,
             help="a file that is parsed by the system"
             )
+
+    open_file_and_parse(args.parse_args().file)
+
+
+def open_file_and_parse(filename: str) -> None:
+    pass
 
 
 if __name__ == "__main__":
