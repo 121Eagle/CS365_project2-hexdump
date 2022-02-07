2eafb7f13d83178418980d2d0856fc5042948ce0
William Mironchuk <wmironchuk@umass.edu>
2022-02-07 10:12:45 -0500

    added the arguments to the hexdump that will be parsed
    The one MANDITORY argument that needs to be parsed is the name of the
    file that is going to be read.

--- hexdump.py
+++ hexdump.py
@@ -1,9 +1,15 @@
-from argparse import ArgumentParser
+from argparse import ArgumentParser, FileType
 
 
 def main():
     args = ArgumentParser(
             discription="display file contents in headecimal and ascii"
+            )
+
+    args.add_argument(
+            "file",
+            type=FileType,
+            help="a file that is parsed by the system"
             )
 
 
