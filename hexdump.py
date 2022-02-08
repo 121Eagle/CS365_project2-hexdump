aa1e3e955d1791fda800ed1b7c4ec2e005a44181
William Mironchuk <wmironchuk@umass.edu>
2022-02-08 09:07:37 -0500

    to some progress here, but most will be overwritten later

--- hexdump.py
+++ hexdump.py
@@ -1,5 +1,6 @@
 from argparse import ArgumentParser
 from io import BufferedReader
+from itertools import count
 
 
 def main():
@@ -35,7 +36,21 @@ def _parse_file(Data: BufferedReader) -> None:
     parses the data of the file for as long as there is data
     to parse
     """
+    for bytes_counted in count(step=16):
+        byte_values = data.read(16)
+
+
+def string_processing(Data: bytes) -> str:
+    """
+    Returns a string value that is what the bytes are as interpreted
+    as a string
+    """
     pass
+
+
+def hexdata_print(data: bytes) -> list[int]:
+
+    return [int(byte, base=16) for byte in data]
 
 
 def _format_text_portion(data: bytes) -> str:
